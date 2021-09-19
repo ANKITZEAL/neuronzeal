833e79494b33377d5122bca4362d08b6a138e21c
ANKITZEAL <ankitzeal@example.com>
2021-09-19 20:10:42 +0530

    logging added and progress bar done

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -4,7 +4,11 @@ import pandas as pd
 import joblib #for saving my model as binary file
 from matplotlib.colors import ListedColormap
 import os
+import logging
 plt.style.use("fivethirtyeight") # THIS IS STYLE OF GRAPHS
+
+
+
 def prepare_data(df):
   """it ise used to separate the dependent variables and independent variables
 
@@ -14,6 +18,8 @@ def prepare_data(df):
   Returns:
       tuple: it returns the tuples of dependent variables and independent variables
   """
+  logging.info(" preparing the  data by segregating the dependent variables and independent variables")
+
   X = df.drop("y", axis=1)
 
   y = df["y"]
@@ -26,10 +32,12 @@ def save_model(model, filename):
       model (python object): trained model
       filename (str): path to save the  trained model
   """
+  logging.info("saving the trained model")
   model_dir = "models"
   os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   filePath = os.path.join(model_dir, filename) # model/filename
   joblib.dump(model, filePath)
+  logging.info(f"saved the trained model {filePath}")
 
 
 def save_plot(df, file_name, model):
@@ -41,6 +49,7 @@ def save_plot(df, file_name, model):
       model (python object): trained model
   """
   def _create_base_plot(df):
+    logging.info("creating the base plot")
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
     plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -48,6 +57,7 @@ def save_plot(df, file_name, model):
     figure.set_size_inches(10, 8)
 
   def _plot_decision_regions(X, y, classfier, resolution=0.02):
+    logging.info("plotting the decision regions")
     colors = ("red", "blue", "lightgreen", "gray", "cyan")
     cmap = ListedColormap(colors[: len(np.unique(y))])
 
@@ -80,3 +90,4 @@ def save_plot(df, file_name, model):
   os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   plotPath = os.path.join(plot_dir, file_name) # model/filename
   plt.savefig(plotPath)
+  logging.info(f"saving the plot at {plotPath}")
